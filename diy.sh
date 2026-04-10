08bebcb2c5a5e855bb687574f3c81bd402c80bf6
z1252 <qoqne@outlook.com>
2026-04-10 22:29:14 +0800

    1

--- diy.sh
+++ diy.sh
@@ -1,7 +1,7 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 
 # 编辑默认的lan口ip地址
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 编辑默认的主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -21,3 +21,4 @@ echo 'src-git qbee https://github.com/c0re100/qBittorrent-Enhanced-Edition' >>fe
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
 git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
 echo 'src-git immortalwrt_packages https://github.com/immortalwrt/packages.git;openwrt-25.12' >> feeds.conf.default
+echo 'src-git immortalwrt_luci https://github.com/immortalwrt/luci.git;openwrt-25.12' >> feeds.conf.default
